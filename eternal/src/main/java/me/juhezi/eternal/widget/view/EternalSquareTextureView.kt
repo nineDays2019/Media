8c737832786ec8f866573ab3f3f07e6e377141de
Juhezi <juhezix@163.com>
2019-06-27 16:04:56 +0800

    [Add] EternalSquareTextureView

---
+++ eternal/src/main/java/me/juhezi/eternal/widget/view/EternalSquareTextureView.kt
@@ -0,0 +1,31 @@
+package me.juhezi.eternal.widget.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.TextureView
+
+// 方形 TextureView
+
+class EternalSquareTextureView : TextureView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    )
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        val size = Math.min(width, height)
+        setMeasuredDimension(size, size)
+    }
+
+}
--- orange/src/main/java/com/juhezi/orange/media/gpuimage/filter/FragmentShaderFilter.kt
+++ orange/src/main/java/com/juhezi/orange/media/gpuimage/filter/FragmentShaderFilter.kt
@@ -18,6 +18,8 @@ class FragmentShaderFilter(
     private var uMouseLocation = 0
     private var uTimeLocation = 0
 
+    private var useProjection = false   // 不能使用投影，现阶段的解决办法就是设置 TextureView 的宽高
+
     private val resolutionVertexArray: VertexArray =
         VertexArray(2)
     private val mouseVertexArray: VertexArray =
@@ -38,6 +40,7 @@ class FragmentShaderFilter(
         }
         if (uResolutionLocation >= 0) {
             resolutionVertexArray.updateBuffer(floatArrayOf(outputWidth.toFloat(), outputHeight.toFloat()))
+//            resolutionVertexArray.updateBuffer(getResolutionArray())
             glUniform2fv(uResolutionLocation, 1, resolutionVertexArray.getFloatBuffer())    // count 可以用来赋值数组
         }
 
@@ -47,16 +50,28 @@ class FragmentShaderFilter(
      * 正交投影矩阵
      */
     override fun getDrawMatrix(): FloatArray {
-        val matrix = FloatArray(16)
-        val aspectRatio = if (outputWidth > outputHeight)
-            outputWidth / outputHeight.toFloat() else
-            outputHeight / outputWidth.toFloat()
-        if (outputWidth > outputHeight) {   // Landscape
-            Matrix.orthoM(matrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f)
-        } else {    // Portrait or square
-            Matrix.orthoM(matrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f)
+        return if (useProjection) {
+            val matrix = FloatArray(16)
+            val aspectRatio = if (outputWidth > outputHeight)
+                outputWidth / outputHeight.toFloat() else
+                outputHeight / outputWidth.toFloat()
+            if (outputWidth > outputHeight) {   // Landscape
+                Matrix.orthoM(matrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f)
+            } else {    // Portrait or square
+                Matrix.orthoM(matrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f)
+            }
+            matrix
+        } else {
+            super.getDrawMatrix()
         }
-        return matrix
+    }
+
+    private fun getResolutionArray(): FloatArray {
+        val value = Math.min(
+            outputWidth.toFloat(),
+            outputHeight.toFloat()
+        )
+        return FloatArray(2) { value }
     }
 
     // 传入触摸坐标
