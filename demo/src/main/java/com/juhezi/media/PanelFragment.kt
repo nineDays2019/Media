dd090b8c8e86398707c33ed147a8d47aef167fdd
Juhezi <juhezix@163.com>
2019-08-06 16:46:40 +0800

    [Update] Demo

--- demo/src/main/java/com/juhezi/media/PanelFragment.kt
+++ demo/src/main/java/com/juhezi/media/PanelFragment.kt
@@ -3,7 +3,6 @@ package com.juhezi.media
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,14 +13,12 @@ import com.juhezi.media.capture.CaptureActivity
 import com.juhezi.orange.bridge.OrangeBridge
 import kotlinx.android.synthetic.main.fragment_panel.view.*
 import me.juhezi.eternal.base.BaseFragment
-import me.juhezi.eternal.builder.buildScheme
 import me.juhezi.eternal.extension.checkPermissionWith
 import me.juhezi.eternal.extension.showToast
 import me.juhezi.eternal.global.loge
 import me.juhezi.eternal.global.logi
 import me.juhezi.eternal.global.logw
 import me.juhezi.eternal.router.OriginalPicker
-import me.juhezi.eternal.router.SchemeUtils
 import me.juhezi.eternal.util.UriUtils
 
 class PanelFragment : BaseFragment() {
@@ -70,36 +67,6 @@ class PanelFragment : BaseFragment() {
                 turnTo(CaptureActivity::class.java)
             }
         }
-
-        rootView.findViewById<View>(R.id.camera_scheme).setOnClickListener {
-            SchemeUtils.openScheme(context!!, buildScheme {
-                scheme = this@PanelFragment.getString(R.string.scheme)
-                host = "demo"
-                path = "capture"
-                permission = Manifest.permission.CAMERA // 添加权限
-                appendOrUpdateParam(CAPTURE_FRONT to "0") // 默认为后置摄像头
-            })
-        }
-        rootView.findViewById<View>(R.id.picture_scheme).setOnClickListener {
-            checkPermissionWith(Manifest.permission.READ_EXTERNAL_STORAGE) {
-                val intent = OriginalPicker.getIntent(OriginalPicker.Type.IMAGE)
-                startActivityForResult(intent, 0x124)
-            }
-        }
-        rootView.findViewById<View>(R.id.gpu_image_scheme).setOnClickListener {
-            SchemeUtils.openScheme(
-                context!!,
-                "eternal://demo/gpu_image?$GPU_FRAGMENT=storage/emulated/0/color.glsl"
-            )
-        }
-        rootView.findViewById<View>(R.id.web_scheme).setOnClickListener {
-            SchemeUtils.openScheme(context!!, buildScheme {
-                scheme = this@PanelFragment.getString(R.string.scheme)
-                host = "demo"
-                path = "web"
-                appendOrUpdateParam(WEB_COLOR to Color.RED.toString())
-            })
-        }
         rootView.findViewById<View>(R.id.web).setOnClickListener {
             turnTo(WebActivity::class.java)
         }
@@ -115,12 +82,12 @@ class PanelFragment : BaseFragment() {
                     intent.putExtra(PICTURE_PATH, UriUtils.getPathFromUri(context, uri))
                     startActivity(intent)
                 }
-            } else if (requestCode == 0x124) {
+            } /*else if (requestCode == 0x124) {
                 SchemeUtils.openScheme(
                     context!!,
                     "eternal://demo/picture?$PICTURE_PATH=${UriUtils.getPathFromUri(context, uri)}"
                 )
-            }
+            }*/
         }
     }
 
