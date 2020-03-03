a2c270a11f519d457b8608a55ad087c307d30d0f
nineDays2019 <night_days@163.com>
2020-03-03 11:01:34 +0800

    [Add] #1

--- orange/src/main/cpp/media/mediametadataretriever.cpp
+++ orange/src/main/cpp/media/mediametadataretriever.cpp
@@ -22,3 +22,41 @@ int MediaMetadataRetriever::setDataSource(int fd, int64_t offset, int64_t length
     Mutex::Autolock _l(mLock);
     return ::set_data_source_fd(&state, fd, offset, length);
 }
+
+int MediaMetadataRetriever::getFrameAtTime(int64_t timeUs, int option, AVPacket *pkt) {
+    Mutex::Autolock _l(mLock);
+    return ::get_frame_at_time(&state, timeUs, option, pkt);
+}
+
+int
+MediaMetadataRetriever::getScaledFrameAtTime(int64_t timeUs, int option, AVPacket *pkt, int width,
+                                             int height) {
+    Mutex::Autolock _l(mLock);
+    return ::get_scaled_frame_at_time(&state, timeUs, option, pkt, width, height);
+}
+
+const char *MediaMetadataRetriever::extractMetadata(const char *key) {
+    Mutex::Autolock _l(mLock);
+    return ::extract_metadata(&state, key);
+}
+
+const char *MediaMetadataRetriever::extractMetadataFromChapter(const char *key, int chapter) {
+    Mutex::Autolock _l(mLock);
+    return ::extract_metadata_from_chapter(&state, key, chapter);
+}
+
+int MediaMetadataRetriever::extractAlbumArt(AVPacket *pkt) {
+    Mutex::Autolock _l(mLock);
+    return ::get_embedded_picture(&state, pkt);
+}
+
+int MediaMetadataRetriever::getMetadata(_Bool update_only, _Bool apply_filter,
+                                        AVDictionary **metadata) {
+    Mutex::Autolock _l(mLock);
+    return ::get_metadata(&state, metadata);
+}
+
+int MediaMetadataRetriever::setNativeWindow(ANativeWindow *native_window) {
+    Mutex::Autolock _l(mLock);
+    return ::set_native_window(&state, native_window);
+}
